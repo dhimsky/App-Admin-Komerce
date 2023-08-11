d3bbfc303ecbc9401f35cf7f1ed8cc6eb31a84fb
dhimsky <dhimasafri1523@gmail.com>
2023-08-11 08:10:37 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:komerce/pages/login.dart'; // Ganti dengan path yang sesuai
+
+void main() {
+  debugPaintSizeEnabled = false;
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Login(), // Menggunakan kelas Login yang sudah Anda sediakan
+    );
+  }
+}
