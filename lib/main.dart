364a7db706dcc67db8bcd8761a21894088643bb8
Dito Ardi Pratama <127459191+ditooard@users.noreply.github.com>
2023-08-14 13:58:47 +0700

    dashboard
    db

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        fontFamily: 'PlusJakartaSans',
+        fontFamily: 'PlusJakartaSans'
       ),
       home: Login(),
       initialRoute: '/login',
--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:komerce/theme.dart';
 
@@ -5,55 +6,60 @@ class DashboardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home), label: 'Beranda'),
+        BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.list_bullet), label: 'Log Aktivitas'),
+        BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.profile_circled), label: 'Profile'),
+      ]),
       backgroundColor: orangeColor,
       body: SafeArea(
-          child: SingleChildScrollView(
+        child: SingleChildScrollView(
         child: Column(
           children: [
             SizedBox(
               height: 30,
             ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 24),
-              child: Row(
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Halo Arief,',
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                          color: whiteColor,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 4,
-                      ),
-                      Text(
-                        'Selamat Datang !',
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600,
-                            color: whiteColor),
-                      )
-                    ],
-                  ),
-                  Spacer(),
-                  Container(
-                    decoration: BoxDecoration(
+            Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Halo Arief,',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
                         color: whiteColor,
-                        borderRadius: BorderRadius.circular(40)),
-                    padding: EdgeInsets.all(12),
-                    child: Icon(
-                      Icons.notifications_none_rounded,
-                      color: Color(0xff626262),
-                      size: 30,
+                      ),
                     ),
+                    SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      'Selamat Datang !',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                          color: whiteColor),
+                    )
+                  ],
+                ),
+                Spacer(),
+                Container(
+                  decoration: BoxDecoration(
+                      color: whiteColor,
+                      borderRadius: BorderRadius.circular(40)),
+                  padding: EdgeInsets.all(12),
+                  child: Icon(
+                    Icons.notifications_none_rounded,
+                    color: Color(0xff626262),
+                    size: 30,
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
             SizedBox(
               height: 21,
@@ -69,10 +75,10 @@ class DashboardPage extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: orangeColor,
-                    ),
+                  Row(
+                    children: [
+                      Container(),
+                    ],
                   ),
                 ],
               ),
