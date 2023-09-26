da6eeb5e76b8be1d58f08a35a4130e4b724e78bd
Dito Ardi Pratama <127459191+ditooard@users.noreply.github.com>
2023-09-26 11:15:45 +0700

    cari order
    sambung api komerce

--- lib/services/order_service.dart
+++ lib/services/order_service.dart
@@ -1,18 +1,40 @@
-import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:komerce/models/order_model.dart';
+import 'package:http/http.dart' as http;
 
-class OrderController {
-  Future<Order?> getOrderById(String orderId) async {
+class OrderService {
+  final String baseUrl = 'https://dev.komship.komerce.my.id/api/v2/admin/order';
+
+  Future<Map<String, dynamic>> getOrderDetails(String orderNumber) async {
+    final bearerToken =
+        'GATEWAYKOMSHIPKOMERCE'; // Ganti dengan token bearer yang sesuai.
     final response = await http.get(
-      Uri.parse('https://650a694edfd73d1fab085713.mockapi.io/order/$orderId'),
+      Uri.parse('$baseUrl/search?search=$orderNumber'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization':
+            'Bearer $bearerToken', // Sesuaikan format header otorisasi.
+      },
     );
 
     if (response.statusCode == 200) {
-      final Map<String, dynamic> data = json.decode(response.body);
-      return Order.fromJson(data);
+      print(
+          'URL yang digunakan: ${Uri.parse('$baseUrl/search?search=$orderNumber')}');
+      print('cek token : $bearerToken');
+      print('Response API: ${response.body}');
+      return jsonDecode(response.body);
+    } else if (response.statusCode == 401) {
+      // Menangani kasus jika status code adalah 401 (Unauthorized)
+      print(
+          'URL yang digunakan: ${Uri.parse('$baseUrl/search?search=$orderNumber')}');
+      print('cek token : $bearerToken');
+      print('Response API: ${response.body}');
+      throw Exception(
+          'Anda tidak memiliki otorisasi untuk mengakses data ini. Silakan login kembali.');
     } else {
-      return null;
+      // Menangani kasus jika status code adalah selain 200 atau 401
+      final errorMessage = jsonDecode(response.body)['message'] ??
+          'Gagal mengambil detail pesanan';
+      throw Exception(errorMessage);
     }
   }
 }
